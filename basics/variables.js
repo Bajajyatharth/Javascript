4acd0be87bd1e7108de4c8462074aac211284455
Bajajyatharth <yatharthbajaj1508@gmail.com>
2024-12-02 19:51:45 +0530

    varaibles and datatypes

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 763487
+let accountEmail = "yashbajaj@gmail.com"
+var accountPass = "64234"
+accountCity ="BKN"
+
+// accountId = 45  not allowed
+accountEmail ="newemail.com"
+accountPass ="7432"
+accountCity ="jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity])
